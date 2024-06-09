898d0cf3f6d07f70b80ee7aee05b7d12014751b6
mineninja77777 <116883276+mineninja77777@users.noreply.github.com>
2024-06-09 14:42:08 +0100

    create ast types adn ifaces

---
+++ ast.ts
@@ -0,0 +1,35 @@
+type NodeType = 
+    | "Program" 
+    | "NumericLiteral" 
+    | "Identifier" 
+    | "BinaryExpr";
+
+// pretty much abstract
+interface Stmt {
+    kind: NodeType;
+}
+
+interface Program extends Stmt{
+    kind: "Program";
+    body: Stmt[];
+}
+
+interface Expr extends Stmt{}
+
+// baisically a two input func
+interface BinaryExpr extends Expr{
+    kind: "BinaryExpr";
+    left: Expr;
+    right: Expr;
+    operator: string;
+}
+
+interface Identifier extends Expr{
+    kind: "Identifier";
+    symbol: string;
+}
+
+interface NumericLiteral extends Expr {
+    kind: "NumericLiteral";
+    value: number;
+}
